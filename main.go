1a0ed30b50f73bcb793abebac20e541c3f8ae736
Seth <thatguythatsbadwithnames@gmail.com>
2020-05-14 13:44:55 -0400

    finished HTTP server section

--- main.go
+++ main.go
@@ -5,14 +5,28 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
+//InMemoryPlayerStore stores player scores in memory
+type InMemoryPlayerStore struct {
+	store map[string]int
+}
 
+//NewInMemoryPlayerStore creates a new InMemoryPlayerStore
+func NewInMemoryPlayerStore() *InMemoryPlayerStore {
+	return &InMemoryPlayerStore{map[string]int{}}
+}
+
+//RecordWin adds a point to the player's score
+func (i *InMemoryPlayerStore) RecordWin(name string) {
+	i.store[name]++
+}
+
+//GetPlayerScore retrieves the given player's score
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
+	return i.store[name]
 }
 
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
--- server.go
+++ server.go
@@ -9,6 +9,7 @@ import (
 //PlayerStore stores player info
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
 }
 
 //PlayerServer uses/stores a PlayerStore
@@ -20,13 +21,27 @@ type PlayerServer struct {
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
 
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
 
 //GetPlayerScore retrieves player score form player store
